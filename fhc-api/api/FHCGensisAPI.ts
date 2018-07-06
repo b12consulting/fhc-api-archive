26ba76ff3535b1d97592c5e3b7853e3c9621c884
Olivier Martin <olivier.martin@b12-consulting.com>
2018-07-06 15:29:37 +0200

    Add Genins API for insurability

---
+++ fhc-api/api/FHCGensisAPI.ts
@@ -0,0 +1,45 @@
+import { XHR } from './XHR';
+
+export class FHCGensisApi {
+  host: string;
+  headers: Array<XHR.Header>;
+
+  constructor(host: string, headers: any) {
+    this.host = host;
+    this.headers = Object.keys(headers).map(k => new XHR.Header(k, headers[k]));
+  }
+
+  handleError(e: XHR.Data) {
+    if (e.status == 401) throw Error('auth-failed');
+    else throw Error('api-error' + e.status);
+  }
+
+  getGeneralInsurabilityUsingGET(
+    ssin,
+    tokenId,
+    keystoreId,
+    passPhrase,
+    hcpNihii, //inami
+    hcpSsin,
+    hcpName,
+    hcpQuality?, //doctor, dentiste
+    date?,
+    hospitalized?
+  ) {
+    const _url =
+      this.host +
+      `/genins/${ssin}` +
+      '?ts=' +
+      new Date().getTime() +
+      (tokenId ? '&tokenId=' + tokenId : '') +
+      (keystoreId ? '&keystoreId=' + keystoreId : '') +
+      (passPhrase ? '&passPhrase=' + encodeURIComponent(passPhrase) : '') +
+      (hcpNihii ? '&hcpNihii=' + hcpNihii : '') +
+      (hcpSsin ? '&hcpSsin=' + hcpSsin : '') +
+      (hcpName ? '&hcpName=' + hcpName : '') +
+      (hcpQuality ? '&hcpQuality=' + hcpQuality : '') +
+      (date ? '&date=' + date : '') +
+      (hospitalized ? '&hospitalized=' + hospitalized : '');
+    return XHR.sendCommand('GET', _url, this.headers).catch(err => this.handleError(err));
+  }
+}
--- fhc-api/fhcApi.ts
+++ fhc-api/fhcApi.ts
@@ -1,1 +1,2 @@
 export * from './api/FHCStsApi';
+export * from './api/FHCGensisAPI';
