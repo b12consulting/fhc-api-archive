a4ff4346969be7528d97e40242efd4fdc4d6d4b1
Olivier Martin <olivier.martin@b12-consulting.com>
2018-07-20 15:44:23 +0200

    add notifyDmg and consultDmg in FHCDmg

--- fhc-api/api/FHCDmgApi.ts
+++ fhc-api/api/FHCDmgApi.ts
@@ -14,6 +14,79 @@ export class FHCDmgApi {
     else throw Error('api-error' + e.status);
   }
 
+  consultDmg(
+    keystoreId,
+    tokenId,
+    passPhrase,
+    hcpNihii,
+    hcpSsin,
+    hcpFirstName,
+    hcpLastName,
+    patientSsin?,
+    patientGender?,
+    oa?,
+    regNrWithMut?,
+    requestDate?
+  ) {
+    const _url =
+      this.host +
+      '/gmd' +
+      '?ts=' +
+      new Date().getTime() +
+      (keystoreId ? '&keystoreId=' + keystoreId : '') +
+      (tokenId ? '&tokenId=' + tokenId : '') +
+      (passPhrase ? '&passPhrase=' + passPhrase : '') +
+      (hcpNihii ? '&hcpNihii=' + hcpNihii : '') +
+      (hcpSsin ? '&hcpSsin=' + hcpSsin : '') +
+      (hcpFirstName ? '&hcpFirstName=' + hcpFirstName : '') +
+      (hcpLastName ? '&hcpLastName=' + hcpLastName : '') +
+      (patientSsin ? '&patientSsin=' + patientSsin : '') +
+      (patientGender ? '&patientGender=' + patientGender : '') +
+      (oa ? '&oa=' + oa : '') +
+      (regNrWithMut ? '&regNrWithMut=' + regNrWithMut : '') +
+      (requestDate ? '&requestDate=' + requestDate : '');
+    return XHR.sendCommand('GET', _url, this.headers).catch(err => this.handleError(err));
+  }
+
+  notifyDmg(
+    keystoreId,
+    tokenId,
+    passPhrase,
+    hcpNihii,
+    hcpSsin,
+    hcpFirstName,
+    hcpLastName,
+    nomenclature,
+    patientSsin?,
+    oa?,
+    regNrWithMut?,
+    patientFirstName?,
+    patientLastName?,
+    patientGender?,
+    requestDate?
+  ) {
+    const _url =
+      this.host +
+      `/gmd/notify/${nomenclature}` +
+      '?ts=' +
+      new Date().getTime() +
+      (keystoreId ? '&keystoreId=' + keystoreId : '') +
+      (tokenId ? '&tokenId=' + tokenId : '') +
+      (passPhrase ? '&passPhrase=' + passPhrase : '') +
+      (hcpNihii ? '&hcpNihii=' + hcpNihii : '') +
+      (hcpSsin ? '&hcpSsin=' + hcpSsin : '') +
+      (hcpFirstName ? '&hcpFirstName=' + hcpFirstName : '') +
+      (hcpLastName ? '&hcpLastName=' + hcpLastName : '') +
+      (patientSsin ? '&patientSsin=' + patientSsin : '') +
+      (oa ? '&oa=' + oa : '') +
+      (regNrWithMut ? '&regNrWithMut=' + regNrWithMut : '') +
+      (patientFirstName ? '&patientFirstName=' + patientFirstName : '') +
+      (patientLastName ? '&patientLastName=' + patientLastName : '') +
+      (patientGender ? '&patientGender=' + patientGender : '') +
+      (requestDate ? '&requestDate=' + requestDate : '');
+    return XHR.sendCommand('POST', _url, this.headers).catch(err => this.handleError(err));
+  }
+
   postDmgsListRequestUsingPOST(
     keystoreId,
     tokenId,
@@ -22,8 +95,8 @@ export class FHCDmgApi {
     hcpSsin,
     hcpFirstName,
     hcpLastName,
-    oa,
-    requestDate
+    oa?,
+    requestDate?
   ) {
     const _url =
       this.host +
